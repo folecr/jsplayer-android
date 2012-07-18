57fce83164ad8bc4790f95fb42166c4ec78d1034
folecr <folecr@gmail.com>
2012-07-18 14:46:59 -0700

    cocosdenshion_bindings should not be necessary in module jsbindings

--- jni/jsbindings/Android.mk
+++ jni/jsbindings/Android.mk
@@ -13,7 +13,6 @@ LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
 LOCAL_WHOLE_STATIC_LIBRARIES += autogentestbindings
-LOCAL_WHOLE_STATIC_LIBRARIES += autogencocosdenshionbindings
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -llog
@@ -22,4 +21,3 @@ include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,spidermonkey/android)
 $(call import-module,simple_test_bindings)
-$(call import-module,cocosdenshion_bindings)
