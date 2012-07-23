393f75df64f1c8ef77a021b3aa639d42f62c5210
folecr <folecr@gmail.com>
2012-07-23 16:04:37 -0700

    Switch to latest helper.js (currently named helperx.js)

--- assets/javascript-spidermonkey/helperx.js
+++ assets/javascript-spidermonkey/helperx.js
@@ -1,25 +1,15 @@
-//
-// http://www.cocos2d-iphone.org
-// http://www.cocos2d-html5.org
-// http://www.cocos2d-x.org
-//
-// Javascript + CocosDenshion tests
-//
-//
-
 // cocos2d Helper
 //function ccp(x, y)
 //{
-//  var floats = new Float32Array(2);
-//  floats[0] = x;
-//  floats[1] = y;
+//	var floats = new Float32Array(2);
+//	floats[0] = x;
+//	floats[1] = y;
 //
-//  return floats;
+//	return floats;
 //}
 
-log("cocos2d-x helper...");
+//var cc = cc || {};
 
-var cc = cc || {};
 cc.log = log;
 
 cc.p = function (x, y) {
@@ -86,16 +76,26 @@ cc.WHITE = cc.c3(255,255,255);
 
 cc.POINT_ZERO = cc.p(0,0);
 
-cc._reuse_point = cc.p(0,0);
+cc._reuse_p0 = cc.p(0,0);
+cc._reuse_p1 = cc.p(0,0);
+cc._reuse_p_index = 0;
 cc._reuse_color3b = cc.c3(255, 255, 255 );
 cc._reuse_color4b = cc.c4(255, 255, 255, 255 );
 cc._reuse_grid = cc.g(0,0);
 
 cc._p = function( x, y )
 {
-    cc._reuse_point[0] = x;
-    cc._reuse_point[1] = y;
-    return cc._reuse_point;
+    if( cc._reuse_p_index == 0 ) {
+        cc._reuse_p0[0] = x;
+        cc._reuse_p0[1] = y;
+        cc._reuse_p_index = 1;
+        return cc._reuse_p0;
+    } else {
+        cc._reuse_p1[0] = x;
+        cc._reuse_p1[1] = y;
+        cc._reuse_p_index = 0;
+        return cc._reuse_p1;
+    }
 }
 
 cc._g = function( x, y )
@@ -124,32 +124,107 @@ cc._c4 = function( r, g, b, a )
 
 cc.rect = function(x,y,w,h)
 {
-    var rect = new Float32Array(4)
-    rect[0] = x;
-    rect[1] = y;
-    rect[2] = w;
-    rect[3] = h;
-    return rect;
+    var platform = __getPlatform();
+    if( platform.substring(0,7) == 'desktop' )
+        var rect = new Float64Array(4)
+    else
+        var rect = new Float32Array(4)
+
+	rect[0] = x;
+	rect[1] = y;
+	rect[2] = w;
+	rect[3] = h;
+	return rect;
 }
 
 cc.size = function(w,h)
 {
-    var size = new Float32Array(2)
-    size[0] = w;
-    size[1] = h;
-    return size;
+    var platform = __getPlatform();
+    if( platform.substring(0,7) == 'desktop' )
+        var size = new Float64Array(2)
+    else
+        var size = new Float32Array(2)
+	size[0] = w;
+	size[1] = h;
+	return size;
 }
 
 cc.size_get_width = function (size )
 {
-    return size[0];
+	return size[0];
 }
 
 cc.size_get_height = function (size )
 {
-    return size[1];
+	return size[1];
 }
 
+cc.Touch.prototype.getLocation = function () {
+    var pt = this.locationInView();
+    return cc.Director.sharedDirector().convertToGL(pt);
+};
+
+cc.Touch.prototype.getDelta = function () {
+    var current = this.locationInView();
+    var last = this.previousLocationInView();
+    cc._reuse_point[0] = current[0] - last[0];
+    cc._reuse_point[1] = last[1] - current[1];
+    return cc._reuse_point;
+};
+
+
+//
+// Google "subclasses"
+// borrowed from closure library
+//
+var goog = goog || {}; // Check to see if already defined in current scope
+goog.inherits = function (childCtor, parentCtor) {
+	/** @constructor */
+	function tempCtor() {};
+	tempCtor.prototype = parentCtor.prototype;
+	childCtor.superClass_ = parentCtor.prototype;
+	childCtor.prototype = new tempCtor();
+	childCtor.prototype.constructor = childCtor;
+
+	// Copy "static" method, but doesn't generate subclasses.
+//	for( var i in parentCtor ) {
+//		childCtor[ i ] = parentCtor[ i ];
+//	}
+};
+goog.base = function(me, opt_methodName, var_args) {
+	var caller = arguments.callee.caller;
+	if (caller.superClass_) {
+		// This is a constructor. Call the superclass constructor.
+		ret =  caller.superClass_.constructor.apply( me, Array.prototype.slice.call(arguments, 1));
+
+		// XXX: SpiderMonkey bindings extensions
+//		__associateObjWithNative( me, ret );
+		return ret;
+	}
+
+	var args = Array.prototype.slice.call(arguments, 2);
+	var foundCaller = false;
+	for (var ctor = me.constructor;
+		 ctor; ctor = ctor.superClass_ && ctor.superClass_.constructor) {
+		if (ctor.prototype[opt_methodName] === caller) {
+			foundCaller = true;
+		} else if (foundCaller) {
+			return ctor.prototype[opt_methodName].apply(me, args);
+		}
+	}
+
+	// If we did not find the caller in the prototype chain,
+	// then one of two things happened:
+	// 1) The caller is an instance method.
+	// 2) This method was not called by the right caller.
+	if (me[opt_methodName] === caller) {
+		return me.constructor.prototype[opt_methodName].apply(me, args);
+	} else {
+		throw Error(
+					'goog.base called from a method of one name ' +
+					'to a method of a different name');
+	}
+};
 
 
 //
@@ -158,107 +233,73 @@ cc.size_get_height = function (size )
 
 cc.Layer.extend = function (prop) {
     var _super = this.prototype;
-	
+
     // Instantiate a base class (but only create the instance,
     // don't run the init constructor)
     initializing = true;
     var prototype = new this();
     initializing = false;
-    fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-	
-	
+//    fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+
     // Copy the properties over onto the new prototype
     for (var name in prop) {
         // Check if we're overwriting an existing function
         prototype[name] = typeof prop[name] == "function" &&
-		typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-		(function (name, fn) {
-		 return function () {
-		 var tmp = this._super;
-		 
-		 // Add a new ._super() method that is the same method
-		 // but on the super-class
-		 this._super = _super[name];
-		 
-		 // The method only need to be bound temporarily, so we
-		 // remove it when we're done executing
-		 var ret = fn.apply(this, arguments);
-		 this._super = tmp;
-		 
-		 return ret;
-		 };
-		 })(name, prop[name]) :
-		prop[name];
+            typeof _super[name] == "function" ?
+            (function (name, fn) {
+                return function () {
+                    var tmp = this._super;
+
+                    // Add a new ._super() method that is the same method
+                    // but on the super-class
+                    this._super = _super[name];
+
+                    // The method only need to be bound temporarily, so we
+                    // remove it when we're done executing
+                    var ret = fn.apply(this, arguments);
+                    this._super = tmp;
+
+                    return ret;
+                };
+            })(name, prop[name]) :
+            prop[name];
     }
-	
+
     // The dummy class constructor
     function Class() {
         // All construction is actually done in the init method
         if (!initializing && this.ctor)
             this.ctor.apply(this, arguments);
     }
-	
+
     // Populate our constructed prototype object
     Class.prototype = prototype;
-	
+
     // Enforce the constructor to be what we expect
     Class.prototype.constructor = Class;
-	
+
     // And make this class extendable
     Class.extend = arguments.callee;
-	
+
     return Class;
 };
 
 cc.LayerGradient.extend = cc.Layer.extend;
 
-var goog = goog || {}; // Check to see if already defined in current scope
-goog.inherits = function (childCtor, parentCtor) {
-    /** @constructor */
-    function tempCtor() {};
-    tempCtor.prototype = parentCtor.prototype;
-    childCtor.superClass_ = parentCtor.prototype;
-    childCtor.prototype = new tempCtor();
-    childCtor.prototype.constructor = childCtor;
+//
+// Chipmunk helpers
+//
+var cp = cp || {};
 
-    // Copy "static" method, but doesn't generate subclasses.
-//  for( var i in parentCtor ) {
-//      childCtor[ i ] = parentCtor[ i ];
-//  }
-};
-goog.base = function(me, opt_methodName, var_args) {
-    var caller = arguments.callee.caller;
-    if (caller.superClass_) {
-        // This is a constructor. Call the superclass constructor.
-        ret =  caller.superClass_.constructor.apply( me, Array.prototype.slice.call(arguments, 1));
+cp.vzero  = cc.p(0,0);
+cp._v = cc._p
 
-        // XXX: SpiderMonkey bindings extensions
-//      __associateObjWithNative( me, ret );
-        return ret;
-    }
 
-    var args = Array.prototype.slice.call(arguments, 2);
-    var foundCaller = false;
-    for (var ctor = me.constructor;
-         ctor; ctor = ctor.superClass_ && ctor.superClass_.constructor) {
-        if (ctor.prototype[opt_methodName] === caller) {
-            foundCaller = true;
-        } else if (foundCaller) {
-            return ctor.prototype[opt_methodName].apply(me, args);
-        }
-    }
-
-    // If we did not find the caller in the prototype chain,
-    // then one of two things happened:
-    // 1) The caller is an instance method.
-    // 2) This method was not called by the right caller.
-    if (me[opt_methodName] === caller) {
-        return me.constructor.prototype[opt_methodName].apply(me, args);
-    } else {
-        throw Error(
-                    'goog.base called from a method of one name ' +
-                    'to a method of a different name');
-    }
-};
-
-log("... cocos2d-x helper done.");
+var gl = gl || {};
+gl.NEAREST = 0x2600;
+gl.LINEAR = 0x2601;
+gl.REPEAT = 0x2901;
+gl.CLAMP_TO_EDGE = 0x812F;
+gl.CLAMP_TO_BORDER = 0x812D;
+gl.LINEAR_MIPMAP_NEAREST = 0x2701;
+gl.GL_NEAREST_MIPMAP_NEAREST = 0x2700;
