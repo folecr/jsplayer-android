a2f90a829a978f1221385989a3a26e8bc07d7a62
folecr <folecr@gmail.com>
2012-08-06 16:14:38 -0700

    Mo better build_native.sh

--- build_native.sh
+++ build_native.sh
@@ -1,43 +1,87 @@
-# set params
-NDK_ROOT_LOCAL=/path/to/android/ndk
-CLANG_ROOT_LOCAL=/path/to/clang+llvm-3.1
+APPNAME="jsplayer-android"
 
-# try to get global variable
-if [ $NDK_ROOT"aaa" != "aaa" ]; then
-    NDK_ROOT_LOCAL=$NDK_ROOT
-    echo "use global definition of NDK_ROOT: $NDK_ROOT"
-fi
+# options
 
-if [ $CLANG_ROOT"aaa" != "aaa" ]; then
-    CLANG_ROOT_LOCAL=$CLANG_ROOT
-    echo "use global definition of CLANG_ROOT: $CLANG_ROOT"
-fi
+debug=
+verbose=
 
 usage(){
 cat << EOF
 usage: $0 [options]
 
-Build C/C++ native code using Android NDK
+Build C/C++ code for $APPNAME using Android NDK
 
 OPTIONS:
-   -h	this help
+-d	Build in debug mode
+-v  Turn on verbose output
+-h	this help
 EOF
 }
 
-# ------------
+while getopts "dvh" OPTION; do
+case "$OPTION" in
+d)
+debug=1
+;;
+v)
+verbose=1
+;;
+h)
+usage
+exit 0
+;;
+esac
+done
 
-# hardcoded
-GAME_ROOT=$PWD
-MOZILLA_ROOT=$PWD/submodules/mozilla-prebuilt
-CXX_GENERATOR_ROOT=$PWD/submodules/cocos2d-x/js/cxx-generator
-COCOS2DX_ROOT=$PWD/submodules/cocos2d-x
+# paths
 
-echo "GAME_ROOT: $GAME_ROOT"
+if [ -z "${NDK_ROOT+aaa}" ]; then
+# ... if NDK_ROOT is not set, use "$HOME/bin/android-ndk"
+    NDK_ROOT="$HOME/bin/android-ndk"
+fi
+
+if [ -z "${CLANG_ROOT+aaa}" ]; then
+    CLANG_ROOT="$HOME/bin/clang+llvm-3.1"
+fi
+
+if [ -z "${PYTHON_BIN+aaa}" ]; then
+# ... if PYTHON_BIN is not set, use "/usr/bin/python2.7"
+    PYTHON_BIN="/usr/bin/python2.7"
+fi
+
+# paths with defaults hardcoded to relative paths
+
+if [ -z "${APP_ROOT+aaa}" ]; then
+    APP_ROOT="$PWD"
+fi
+
+if [ -z "${MOZILLA_ROOT+aaa}" ]; then
+    MOZILLA_ROOT="$PWD/submodules/mozilla-prebuilt"
+fi
+
+if [ -z "${CXX_GENERATOR_ROOT+aaa}" ]; then
+    CXX_GENERATOR_ROOT="$PWD/submodules/cxx-generator"
+fi
+
+echo "CLANG_ROOT: $CLANG_ROOT"
+echo "NDK_ROOT: $NDK_ROOT"
+echo "APP_ROOT: $APP_ROOT"
 echo "MOZILLA_ROOT: $MOZILLA_ROOT"
 echo "CXX_GENERATOR_ROOT: $CXX_GENERATOR_ROOT"
+echo "PYTHON_BIN: $PYTHON_BIN"
 
-# Currently we only have the android game
-GAME_ANDROID_ROOT=$GAME_ROOT
+# Currently we only have the android app
+APP_ANDROID_ROOT="$APP_ROOT"
+
+DEBUG_OPTIONS=
+if [[ $debug ]]; then
+    DEBUG_OPTIONS="NDK_DEBUG=1"
+fi
+
+VERBOSE_OPTIONS=
+if [[ $verbose ]]; then
+    VERBOSE_OPTIONS="NDK_LOG=1 V=1"
+fi
 
 # Generate bindings for simpletest
 echo "Generating bindings..."
@@ -46,25 +90,13 @@ set -x
 pwd
 pushd $PWD
 cd ${CXX_GENERATOR_ROOT}
-DYLD_LIBRARY_PATH=${CLANG_ROOT_LOCAL}/lib /opt/local/bin/python2.7 ${CXX_GENERATOR_ROOT}/generator.py ${GAME_ANDROID_ROOT}/custom.ini -s test -o ${GAME_ANDROID_ROOT}/jni/simple_test_bindings
+DYLD_LIBRARY_PATH=${CLANG_ROOT}/lib $PYTHON_BIN ${CXX_GENERATOR_ROOT}/generator.py ${APP_ANDROID_ROOT}/test.ini -s test -o ${APP_ANDROID_ROOT}/jni/simple_test_bindings
 popd
 pwd
 set +x
 
-# # Generate bindings for cocosdenshion
-# echo "Generating bindings..."
-# echo "change directory to the generator to run it..."
-# set -x
-# pwd
-# pushd $PWD
-# cd ${CXX_GENERATOR_ROOT}
-# DYLD_LIBRARY_PATH=${CLANG_ROOT_LOCAL}/lib /opt/local/bin/python2.7 ${CXX_GENERATOR_ROOT}/generator.py ${GAME_ANDROID_ROOT}/cocosdenshion.ini -s cocosdenshion -o ${GAME_ANDROID_ROOT}/jni/cocosdenshion_bindings
-# popd
-# pwd
-# set +x
-
 # build
 echo "Building native code..."
-$NDK_ROOT_LOCAL/ndk-build -C $GAME_ANDROID_ROOT \
-    NDK_MODULE_PATH=${GAME_ANDROID_ROOT}/jni:${MOZILLA_ROOT}:${CXX_GENERATOR_ROOT}:${COCOS2DX_ROOT}:${COCOS2DX_ROOT}/cocos2dx/platform/third_party/android/prebuilt
+$NDK_ROOT/ndk-build -C $APP_ANDROID_ROOT $DEBUG_OPTIONS $VERBOSE_OPTIONS\
+    NDK_MODULE_PATH=${APP_ANDROID_ROOT}/jni:${MOZILLA_ROOT}:${CXX_GENERATOR_ROOT}
 echo "... Building native code : Done."
